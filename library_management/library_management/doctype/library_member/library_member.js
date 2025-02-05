9f14d9fbb54ce553b9df4aee865298990af91d5a
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-02-05 19:15:43 +0530

    Text Added

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -1,20 +1,21 @@
 // Copyright (c) 2025, hemil and contributors
 // For license information, please see license.txt
+// This is Demo Text
 
-frappe.ui.form.on('Library Member', {
+frappe.ui.form.on("Library Member", {
 	refresh(frm) {
-		frm.add_custom_button(__("Library Membership"),function(){
-		    frappe.call({
-		        method:'library_management.library_management.doctype.library_member.library_member.create_record',
-		        args:{
-		            library_member_name:frm.doc.name
-		        },
-		        callback:function(r){
-		            if(r){
-		                frappe.msgprint("New Membership Created")
-		            }
-		        }
-		    })
+		frm.add_custom_button(__("Library Membership"), function () {
+			frappe.call({
+				method: "library_management.library_management.doctype.library_member.library_member.create_record",
+				args: {
+					library_member_name: frm.doc.name,
+				},
+				callback: function (r) {
+					if (r) {
+						frappe.msgprint("New Membership Created");
+					}
+				},
+			});
 		});
-	}
-})
+	},
+});

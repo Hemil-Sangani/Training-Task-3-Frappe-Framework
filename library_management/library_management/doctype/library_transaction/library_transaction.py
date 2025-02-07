e75bdd951ee3fd22356168b6552f814106081d7e
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-02-07 18:41:30 +0530

    Add Controller on Library Transaction

--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2025, hemil and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LibraryTransaction(Document):
-	pass
+	def before_insert(self):
+		frappe.msgprint(f"You Added a new Transaction with {self.name} name")
+
+	def on_submit(self):
+		frappe.msgprint("You Succesfully Submitted the Transaction")

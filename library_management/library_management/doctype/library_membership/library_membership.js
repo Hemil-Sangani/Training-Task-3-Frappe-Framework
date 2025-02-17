c25cdb856e4af8e3f81acbf0707f34fff34fbe7b
Hemil-Sangani <hemil@sanskartechnolab.com>
2025-02-17 18:59:34 +0530

    Add Workspace to Module and add controller method on Library membership Doctype using JS

--- library_management/library_management/doctype/library_membership/library_membership.js
+++ library_management/library_management/doctype/library_membership/library_membership.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2025, hemil and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Library Membership", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Library Membership", {
+    refresh: function(frm) {
+        // You can add refresh logic here
+    },
+    on_submit: function(frm) {
+        frappe.msgprint("You Successfully Created the Membership");
+    }
+});
--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -3,10 +3,16 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.model.naming import getseries
 
 
 class LibraryTransaction(Document):
-	def before_insert(self):
+	def autoname(self):
+		prefix = 'LT-{}-'.format(self.library_member)
+		series_number = getseries(prefix, 3)
+		self.name = f"{prefix}{series_number}"  
+	
+	def after_insert(self):
 		frappe.msgprint(f"You Added a new Transaction with {self.name} name")
 
 	def on_submit(self):
